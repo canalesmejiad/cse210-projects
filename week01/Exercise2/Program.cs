2852c5158e5a224cac30e3b479e1564971250669
canalesmejiad <david.kanale@hotmail.es>
2025-09-05 21:52:19 -0700

    Modifications done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter a grade porcentage:");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
     }
 }
