62e9f4cca4699bd7b919c1020ec70dba241226bc
canalesmejiad <david.kanale@hotmail.es>
2025-09-05 23:31:37 -0700

    Update made

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        Console.WriteLine($"What is the magic number? {number}");
+
+
+        Console.WriteLine("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guessNumber = int.Parse(guess);
+
+        while (guessNumber != number)
+        {
+            if (guessNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.WriteLine("Try again: ");
+            guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+        }
+        Console.WriteLine("You guessed it!!");
     }
 }
