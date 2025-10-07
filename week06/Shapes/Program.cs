059747b77b222449d31b64f0d0529d63039f3e2c
canalesmejiad <david.kanale@hotmail.es>
2025-10-06 20:57:20 -0700

    cm

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea():F2}");
+        }
     }
 }
